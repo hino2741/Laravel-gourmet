85fd80876b8e9e0d0ba783aa0621d4b2bfc07d16
日野大樹 <da@hinodaikinoMacBook-Pro.local>
2024-03-04 00:20:09 +0900

    feat:管理者用インフォメーション一覧画面項目定義。

--- src/app/Infomation.php
+++ src/app/Infomation.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Infomation extends Model
 {
     protected $table = 'infomation';
+
+    protected $dates = [
+        'release_date'
+    ];
 }
--- src/resources/views/auth/index.blade.php
+++ src/resources/views/auth/index.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.admin')
-<h1>test</h1>
+
+@section('content')
+<h1>お客様へのお知らせ一覧画面</h1>
 @foreach ($infomation as $infomation1)
+  <h2>公開日</h2>
+  <div>{{ $infomation1->release_date->format('Y/m/d') }}</div>
+  <h2>タイトル</h2>
   <div>{{ $infomation1->title }}</div>
+  <h2>本文</h2>
+  <div>{{ $infomation1->content }}</div>
 @endforeach
