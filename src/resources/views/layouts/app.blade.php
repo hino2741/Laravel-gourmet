1d1a80ee41f909a8174830885fed717636eda645
日野大樹 <da@hinodaikinoMacBook-Pro.local>
2024-03-08 06:52:21 +0900

    style:レビュー修正・インデント、空行追加

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -48,15 +48,15 @@
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     @isset($authgroup)
-                                    {{ Auth::guard($authgroup)->user()->name }} <span class="caret"></span>
+                                        {{ Auth::guard($authgroup)->user()->name }} <span class="caret"></span>
                                     @else
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                        {{ Auth::user()->name }} <span class="caret"></span>
                                     @endisset
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -69,7 +69,6 @@
                 </div>
             </div>
         </nav>
-
         <main class="py-4">
             @yield('content')
         </main>
